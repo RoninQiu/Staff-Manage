ac063403c48147fa6a5ea3f50f1fb1d39d1de103
Your NameRonin <you@example.com>
2021-04-08 14:56:31 +0800

    first commit

---
+++ Management_System.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include "workerManager.h"
+
+
+int main(){
+    //实例化
+    WorkerManager wm;
+
+    wm.Show_Menu();
+    
+    system("pause");
+
+    return 0;
+}
---
+++ workerManager.h
@@ -0,0 +1,14 @@
+#pragma once
+//防止头文件重复
+#include <iostream>
+
+using namespace std;
+
+class WorkerManager {
+public:
+    WorkerManager();
+
+    void Show_Menu();
+
+    ~WorkerManager();
+};
